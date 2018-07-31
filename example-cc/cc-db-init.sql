4da4300e58b49471b156ce7854106f34d38a338e
shmilee <shmilee.zju@gmail.com>
2018-07-31 19:19:58 +0800

    table submitter, citizen, activity, favorite

--- example-cc/cc-db-init.sql
+++ example-cc/cc-db-init.sql
@@ -1,49 +1,89 @@
 -- MySQL Distrib 10.2.15-MariaDB, for Linux (x86_64)
 -- Host: localhost    Database: cultural_centre
 
-CREATE USER 'ccsetter'@'localhost' IDENTIFIED BY 'setter_pass';
-CREATE USER 'ccgetter'@'localhost' IDENTIFIED BY 'getter_pass';
-
 CREATE DATABASE cultural_centre;
 USE cultural_centre;
 
 --
--- Table structure for table `activities`
--- priority TINYINT, -1:pass, 0:off, 1:on
--- author VARCHAR, name in user table
--- a_type VARCHAR, 类别: 电影 展览 培训 演出 讲座 其他
--- reservation VARCHAR, 预约: 个人 摇号 团体 免预约 其他
+-- Table structure for `submitter`
 --
 
-CREATE TABLE IF NOT EXISTS activities (
-    a_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    priority TINYINT NOT NULL DEFAULT 1,
+CREATE TABLE IF NOT EXISTS `submitter` (
+    sid INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    passwd VARCHAR(100) NOT NULL,
+    sgroup TINYINT UNSIGNED NOT NULL DEFAULT 1,
+    employer VARCHAR(50) NOT NULL,
+    access_token VARCHAR(100) NOT NULL,
+    reserve VARCHAR(100) NOT NULL,
+    CONSTRAINT chk_grp CHECK (sgroup<2)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for `citizen`
+--
+
+CREATE TABLE IF NOT EXISTS `citizen` (
+    cid INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    wx_openid VARCHAR(100) UNIQUE NOT NULL,
+    wx_session_key VARCHAR(100) NOT NULL,
+    access_token VARCHAR(100) NOT NULL,
+    reserve VARCHAR(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for `activity`
+-- priority, 0:off, 1:on
+-- author, name in table `submitter`
+-- a_type, 类别: 电影 展览 培训 演出 讲座 其他
+-- reservation, 预约: 个人 摇号 团体 免预约 其他
+--
+
+CREATE TABLE IF NOT EXISTS `activity` (
+    aid INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    priority TINYINT UNSIGNED NOT NULL DEFAULT 1,
     title VARCHAR(200) NOT NULL,
     author VARCHAR(100) NOT NULL,
     a_host VARCHAR(100) NOT NULL,
     contact VARCHAR(100) NOT NULL,
     submission_datetime DATETIME NOT NULL,
     start_datetime DATETIME NOT NULL,
-    duration TIME,
+    duration TIME NOT NULL,
     longitude DOUBLE NOT NULL,
     latitude DOUBLE NOT NULL,
     location VARCHAR(200) NOT NULL,
     a_type VARCHAR(20) NOT NULL,
     reservation VARCHAR(20) NOT NULL,
     introduction TEXT NOT NULL,
+    reserve VARCHAR(200) NOT NULL,
     CONSTRAINT chk_itude CHECK (longitude<180 AND longitude>-180 AND latitude<90 AND latitude>-90)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-GRANT ALL ON cultural_centre.activities TO 'ccsetter'@'localhost';
-GRANT SELECT ON cultural_centre.activities TO 'ccgetter'@'localhost';
+--
+-- Table structure for `favorite`
+-- citizen <-> activity
+--
 
--- Tests temp items
-INSERT INTO activities (
-    priority, title, author, a_host, contact, submission_datetime, start_datetime, duration, longitude, latitude, location, a_type, reservation, introduction) values (
-    1, '活动名称1', 'sh', '嘉兴文化馆', '电话123456', '2018-07-12 12:12:12', '2018-07-26 10:00:00', '2:00:00', 120.75964, 30.73644, '五星路88号', '教学', '免预约', 'intro'), (
-    2, '活动名称2', 'sh', '嘉兴图书馆', '电话1啊啊23456', '2018-07-12 12:12:12', '2018-07-26 10:00:00', '2:00:00', 120.76442, 30.74467, '五星路88号', '演讲', '免预约', 'intro'), (
-    1, '活动名称3', 'sh', '嘉兴文化馆', '电话x123456', '2018-07-12 12:12:12', '2018-07-25 10:00:00', '2:00:00', 120.75964, 30.73644, '五星路88号', '演讲', '免预约', 'intro'), (
-    1, '活动名称4', 'sh', '嘉兴文化馆', '电话x123333456', '2018-07-12 12:12:12', '2018-08-25 10:00:00', '2:00:00', 120.75964, 30.73644, '五星路88号', '演讲', '免预约', 'intro'), (
-    1, '活动名称6', 'sh', '嘉兴文化馆', '电话123456', '2018-07-12 12:12:12', '2018-07-20 10:00:00', '2:00:00', 120.75964, 30.73644, '五星路88号', '教学', '免预约', 'intro'), (
-    2, '活动名称7', 'sh', '嘉兴图书馆', '电话123456', '2018-07-12 12:12:12', '2018-07-28 10:00:00', '2:00:00', 120.76442, 30.74467, '五星路88号', '演讲', '免预约', '中国古代，玉是沟通天地的灵媒和祭祀鬼神的重器。它既是权势和地位的象征，又是士人君子洁身明志的佩饰，更是丧葬中保护死者的圣物。山西古玉，温润，美丽，典雅，厚重，是中国玉文化的最灿烂的组成部分。'), (
-    1, '活动名称8', 'sh', '嘉兴文化馆', '电话x123456', '2018-07-12 12:12:12', '2018-07-31 10:00:00', '2:00:00', 120.75964, 30.73644, '五星路88号', '演讲', '免预约', 'intro');
+CREATE TABLE IF NOT EXISTS `favorite` (
+    cid INT UNSIGNED,
+    aid INT UNSIGNED,
+    CONSTRAINT pk_ca PRIMARY KEY(cid, aid),
+    CONSTRAINT fk_citizen FOREIGN KEY(cid) REFERENCES citizen(cid),
+    CONSTRAINT fk_activity FOREIGN KEY(aid) REFERENCES activity(aid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- USER
+--
+
+CREATE USER 'ccadmin'@'localhost' IDENTIFIED BY 'ccadmin_pass';
+GRANT ALL ON cultural_centre.* TO 'ccadmin'@'localhost';
+
+CREATE USER 'ccsetter'@'localhost' IDENTIFIED BY 'ccsetter_pass';
+GRANT ALL ON cultural_centre.activity TO 'ccsetter'@'localhost';
+GRANT UPDATE ON cultural_centre.submitter TO 'ccsetter'@'localhost';
+
+CREATE USER 'ccgetter'@'localhost' IDENTIFIED BY 'ccgetter_pass';
+GRANT ALL ON cultural_centre.citizen TO 'ccgetter'@'localhost';
+GRANT SELECT ON cultural_centre.activity TO 'ccgetter'@'localhost';
+GRANT ALL ON cultural_centre.favorite TO 'ccgetter'@'localhost';
