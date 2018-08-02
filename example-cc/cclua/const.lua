a58abe6c2b48222c8fa1cb322e3945fd8f580224
shmilee <shmilee.zju@gmail.com>
2018-08-02 15:01:41 +0800

    cc: add cclua.const.USER

--- example-cc/cclua/const.lua
+++ example-cc/cclua/const.lua
@@ -1,16 +1,12 @@
--- Copyright (c) 2018 shmilee
+-- @Copyright (c) 2018 shmilee
+-- @classmod cclua.const
 
-local _M = { _VERSION = '0.01' }
+local _M = { _VERSION = '0.1.0' }
 
 _M.MYSQL = {
     HOST = "127.0.0.1",
     PORT = 3306,
     DATABASE = "cultural_centre",
-    ACT_TABLE = 'activities',
-    SET_USER = "ccsetter",
-    SET_PASSWORD = "setter_pass",
-    GET_USER = "ccgetter",
-    GET_PASSWORD = "getter_pass",
     DEFAULT_CHARSET = "utf8",
     MAX_PACKET_SIZE = 1024 * 1024,
     TIMEOUT = 2000, -- 2 sec
@@ -20,6 +16,12 @@ _M.REDIS = {
     HOST = "127.0.0.1",
     PORT = 6379,
     TIMEOUT = 1000,
+}
+
+_M.USER = {
+    ccadmin = { passwd = "ccadmin_pass" },
+    ccsetter = { passwd = "ccsetter_pass" },
+    ccgetter = { passwd = "ccgetter_pass" },
 }
 
 return _M
